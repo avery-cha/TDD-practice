87af60e73d9c91ad841e65d8070dbbd3ecfb6b58
Avery <evans.c.avery@gmail.com>
2024-03-18 14:35:17 -0600

    practicing TDD

---
+++ recipe-collection/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [recipeFormShown, showRecipeForm] = useState(false);
+
+  let submitRecipe = (event) => {};
+
+  return (
+    <div className="App">
+      <h1 className="App-header">My Recipes</h1>
+      {
+        recipeFormShown ?
+          <>
+            <form id="recipe-form" name='recipe-form' onSubmit={submitRecipe}>
+              <label htmlFor="newRecipeName">Recipe name: </label>
+              <input type="text" id="newRecipeName" />
+              <label htmlFor="newRecipeInstructions">Instructions:</label>
+              <textarea id="newRecipeInstructions" placeholder="write recipe instructions here..." />
+              <input type="submit" />
+            </form>
+          </>
+          :
+            <button onClick={ () => showRecipeForm(!recipeFormShown) }>Add Recipe</button>
+      }
+    </div>
+  );
+}
+
+export default App;
